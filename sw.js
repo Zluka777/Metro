674855bb0a6a17f9ca203dfad1e96a9fca3e3715
Andrey <chokoboy333@gmail.com>
2018-06-09 11:10:01 +0400

    modif sw

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ importScripts('sw-toolbox.js');
 
 toolbox.precache(["html.html","style.css",]);
 
-toolbox.router.get('/images/*', toolbox.cacheFirst);
+toolbox.router.get('/image/*', toolbox.cacheFirst);
 
 toolbox.router.get('/*', toolbox.networkFirst, {
   networkTimeoutSeconds: 5
